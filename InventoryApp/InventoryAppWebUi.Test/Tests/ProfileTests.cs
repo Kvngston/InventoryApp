e151484ecda16b2a5985fca7a74c04c12911f05d
Nwankwu Tochukwu <t.nwankwu@genesystechhub.com>
2021-03-24 14:27:10 +0100

    updates

---
+++ InventoryApp/InventoryAppWebUi.Test/Tests/ProfileTests.cs
@@ -0,0 +1,20 @@
+﻿using inventoryAppDomain.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace InventoryAppWebUi.Test.Tests
+{
+    [TestFixture]
+    public class ProfileTests
+    {
+        private Mock<IRoleService> _mockRoleService = new Mock<IRoleService>();
+        private Mock<IProfileService> _mockProfileService = new Mock<IProfileService>();
+        private Mock<INotificationService> _mockNotificationService = new Mock<INotificationService>();
+
+        [Test]
+        public void RemoveUser()
+        {
+            
+        }
+    }
+}

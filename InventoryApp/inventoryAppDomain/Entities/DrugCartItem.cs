9cb0b175121676eb297fa0485144d35f7e8436ec
selfmadecode <r.anyanwu@genesystechhub.com>
2021-05-07 11:57:10 +0100

    AddPrescribedToCart

--- InventoryApp/inventoryAppDomain/Entities/DrugCartItem.cs
+++ InventoryApp/inventoryAppDomain/Entities/DrugCartItem.cs
@@ -19,7 +19,8 @@ namespace inventoryAppDomain.Entities
         public int DrugId { get; set; }
         public Drug Drug { get; set; }
 
-        
+        public int PrescribedQuantity { get; set; }
+
         public int DrugCartId { get; set; }
 
         public DrugCart DrugCart { get; set; }
